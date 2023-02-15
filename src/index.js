017560d0bb936a27d86385d23bcacb0bf67402e5
Colby Shabel <jcshabel@gmail.com>
2023-02-14 20:31:44 -0600

    commit tree b690fd6b56617b01f45e753549090e7e5af23612

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    addList(e.target.newtaskdescription.vaule)
+    form.reset()
+  })
+
+
 });
+
+function addList(newtaskdescription){
+let li = document.createElement('li')
+let btn = document.createElement('button')
+btn.addEventListener('click',handleDelete)
+btn.textContent = 'x'
+li.textContent = ` ${newtaskdescription}`
+li.appendChild(btn)
+console.log(li)
+document.querySelector('#task').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
